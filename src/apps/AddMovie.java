2b21ca278a3ce96929b284101d287489d72b86d7
DELL <DELL@192.170.40.111>
2018-10-28 13:42:07 +0530

    first commit1

---
+++ src/apps/AddMovie.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package apps;
+
+import db.dbconnection;
+import javax.swing.JOptionPane;
+class AddMovie {
+
+    static void setVisible(boolean b) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ src/db/dbconnection.java
@@ -0,0 +1,37 @@
+/* 
+
+
+
+
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package db;
+import java.sql.*;
+public class dbconnection
+{
+public Connection con;
+public Statement stmt;
+public PreparedStatement pstmt;
+public ResultSet rst;
+public dbconnection()
+{
+    try
+    {
+        Class.forName("com.mysql.jdbc.Driver");
+        con=DriverManager.getConnection("jdbc:mysql://localhost:3306/moviepro","root","root");
+    }
+    catch(Exception e)
+    {
+        e.printStackTrace();
+    }
+}
+
+    public class pstmdb {
+
+        public pstmdb() {
+        }
+    }
+}
+
